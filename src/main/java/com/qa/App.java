337722fe3dc543eaeb842a34ad67b8844b786c4d
Harvey Ellman-Brown <mharvey.eb@gmail.com>
2023-06-13 12:30:37 +0100

    fixed bug

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.printin(sayHelloToSomeone("Harvey"));
+        System.out.println(sayHelloToSomeone("Harvey"));
         System.out.println(sayGoodbye());
     }
 
